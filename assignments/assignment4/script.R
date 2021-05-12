1207e19b939ab19c0805b3aae956a98abef8d7c8
Will Lowe <conjugateprior@gmail.com>
2021-05-12 17:27:41 +0200

    more more

--- assignments/assignment4/script.R
+++ assignments/assignment4/script.R
@@ -1,3 +1,8 @@
+library(tidyverse)
+library(ggrepel)
+library(ca)
+
+#comparison time
 
 allcmp20 <- read_csv("data/MPDataset_MPDS2020a.csv")
 allcmp20$partyabbrev[allcmp20$partyname == "Pirates"] <- "Piraten"
@@ -5,7 +10,20 @@ allcmp20$partyabbrev[allcmp20$partyname == "Pirates"] <- "Piraten"
 cmp20 <- allcmp20 %>%
   select(edate, countryname, partyname, partyabbrev,
          total, voteper = pervote, uncoded = peruncod,
-         matches("per\\d\\d\\d$")) %>% # per%d%d%d% are subcategories, so we ignore them
+         matches("per\\d\\d\\d$")) # per%d%d%d% are subcategories, so we ignore them
+
+
+filter(cmp20, countryname == "Germany") %>%
+  select(starts_with("per")) %>%
+  summarise(across(.fns = function(x) mean(as.numeric(x != 0))))
+
+filter(cmp20, countryname == "Germany") %>% mutate(edate =  as.Date(edate, format = "%d/%m/%Y")) %>%
+  select(edate,starts_with("per")) %>%
+  pivot_longer(-edate, names_to = "topic") %>%
+  group_by(edate, topic) %>%
+  summarise(mean_prop = mean(value))
+
+cmp20 %>%
   mutate(edate = as.Date(edate, format = "%d/%m/%Y"),
          eyear = lubridate::year(edate), # make a nice year just in case we want to filter with it
          label = paste(partyabbrev, eyear, sep = ":"), # for graphing
@@ -17,7 +35,7 @@ mat <- data.matrix(counts)
 rownames(mat) <- cmp20$label
 colnames(mat) <- itemcodes$name
 
-## Analysis: Germany
+## Analysis: Germany alone
 
 oldest <- as.Date("1990-01-01")
 de_mat <- mat[cmp20$edate > oldest & cmp20$countryname == "Germany",]
@@ -25,6 +43,34 @@ de_rest <- filter(select(cmp20, -starts_with("per")),
                   edate > oldest,
                   cmp20$countryname == "Germany")
 
-library(ca)
-mod1 <- ca(de_mat)
+
+mod_de <- ca(de_mat)
+
+theta_de <- data.frame(partyyear = de_rest$label,
+                       mod_de$rowcoord[,1:2])
+ggplot(theta_de, aes(Dim1, Dim2, label = partyyear)) +
+  geom_point() +
+  geom_text_repel(alpha = 0.5)
+
+# hmm, maybe we'll flip both axes to better correspond to left right
+theta_de <- mutate(theta_de,
+                   Dim1 = -Dim1,
+                   Dim2 = -Dim2)
+ggplot(theta_de, aes(Dim1, Dim2, label = partyyear)) +
+  geom_point() +
+  geom_text_repel(alpha = 0.5)
+
+ggplot(
+
+## Analysis: Germany and Switzerland
+desw_mat <- mat[cmp20$edate > oldest &
+                cmp20$countryname %in% c("Germany", "Switzerland")]
+desw_rest <- filter(select(cmp20, -starts_with("per")),
+                  edate > oldest,
+                  cmp20$countryname %in% c("Germany", "Switzerland"))
+proj <- which(desw_rest$countryname == "Switzerland")
+mod_desw <- ca(desw_mat, suprow = proj)
+
+
+
 
